362f97ef0bc6fbaed0ba1cc7ec895aac073cde6e
Kengo TODA <toda_k@henry.jp>
2022-12-06 15:06:13 +0800

    fix: handle BOM as an empty line
    Signed-off-by: Kengo TODA <toda_k@henry.jp>

--- src/commonMain/kotlin/com/github/doyaaaaaken/kotlincsv/client/CsvFileReader.kt
+++ src/commonMain/kotlin/com/github/doyaaaaaken/kotlincsv/client/CsvFileReader.kt
@@ -24,7 +24,6 @@ class CsvFileReader internal constructor(
     private var rowNum = 0L
 
     private val parser = CsvParser(ctx.quoteChar, ctx.delimiter, ctx.escapeChar)
-    private val BOM = '\uFEFF'
 
     /**
      * read next csv row
@@ -115,7 +114,7 @@ class CsvFileReader internal constructor(
             } else {
                 null
             }
-        } else if (ctx.skipEmptyLine && (nextLine.isBlank() || nextLine == BOM.toString()) && leftOver.isBlank()) {
+        } else if (ctx.skipEmptyLine && nextLine.isBlank() && leftOver.isBlank()) {
             readUntilNextCsvRow(leftOver)
         } else {
             val value = if (leftOver.isEmpty()) {
